aa8da833f85d20bd25a95ef8ca16309535ea7dce
Ximing Peng <ximing.peng@shopee.com>
2020-07-21 11:17:09 +0800

    finish login-ui

--- README.md
+++ README.md
@@ -14,7 +14,25 @@ Start the dev server,
 $ yarn start
 ```
 
+## 项目要求
+
+链接：
+ https://confluence.shopee.io/pages/viewpage.action?pageId=28510146
+ 
+
 ## Notes
 
 1. 基于umijs3脚手架构建项目
 2. 【项目要求】使用typescript + hooks
+
+## 遇到的遗忘知识点和一些问题
+
+1. 关于登录界面遮罩层 与内容层的设置 ：
+    添加遮罩：https://blog.csdn.net/GrootBaby/article/details/86536064 https://blog.csdn.net/mr_fzz/article/details/53219367
+    为什么设置内容曾 position ： relative 就可以让内容不背遮挡了？https://blog.csdn.net/u011456552/article/details/52398159
+2. 颜色代码 转 rgb https://rgb.to/8560a9
+3. svg调整尺寸与颜色
+4. 背景图片自适应宽高
+5. 移动端宽高设置：https://imweb.io/topic/5b8f6a1580495bb24c9217ac 【我用了vw wh.....后面再调整吧】
+6. css元素子元素选择器
+
---
+++ src/pages/login.tsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import loginStyle from './login.less';
+import { InputItem } from 'antd-mobile';
+
+//先就使用React 实现登录页面
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={loginStyle.login_main}>
+        <div className={loginStyle.mask}></div>
+        <div className={loginStyle.content}>
+          <h2>FIND THE MOST LOVED ACTIVITIES</h2>
+          <h3>BLACK CAT</h3>
+          <div className={loginStyle.cat}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              id="Layer_1"
+              data-name="Layer 1"
+              viewBox="0 0 32 32"
+              width="50"
+              height="50"
+            >
+              <title>logo-cat</title>
+              <polygon
+                points="26.47 14.44 23.07 19.93 23.07 27.38 25.83 29.84 19.2 29.84 21.89 27.36 21.89 19.72 15.69 10.95 19.62 10.95 21.48 9.19 18.18 4.17 14.73 3.14 15.15 -0.03 9.92 4.17 2.83 17.38 7.78 28.12 5.51 30.53 5.51 31.97 9.26 31.97 10.18 31.48 10.93 31.97 29.94 31.97 29.94 30.25 25.68 25.99 25.68 20.55 27.96 16.84 28.78 16.84 29.2 20.08 30.4 20.08 30.71 14.44 26.47 14.44"
+                fill="#D5EF7F"
+              />
+            </svg>
+          </div>
+          <div className={loginStyle.input}>
+            <InputItem>
+              <div className={loginStyle.iconimg1}></div>
+            </InputItem>
+            <InputItem type="password">
+              <div className={loginStyle.iconimg2}></div>
+            </InputItem>
+          </div>
+        </div>
+        <button className={loginStyle.login_btn}>SIGN IN</button>
+      </div>
+    );
+  }
+}
+
+export default Login;
