1a2424dcc460304452f8c342d335bef9841e44d1
Ximing Peng <ximing.peng@shopee.com>
2020-07-21 12:16:38 +0800

    初始化列表页面

---
+++ src/pages/lists.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Lists extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Lists;
